3a3e8126e0518bfb604826a6af0cb62405a99fdf
Dakota Miller <ddmiller1995@gmail.com>
2015-12-03 12:17:51 -0800

    Add basic chatroom

--- sockets/server.js
+++ sockets/server.js
@@ -3,3 +3,47 @@
     main server script for the socket.io chat demo
 */
 
+var net = require('net');
+
+var server = net.createServer();
+
+var clients = [];
+
+server.on('connection', function(socket) {
+    console.log('connection received');
+
+    client.push(socket);
+
+    var broadcast = function(name, message) {
+        clients.forEach(function(client) {
+            client.write('[' + name + ']' + message + '\n');
+        });
+    }
+
+    var name;
+
+    socket.write('What is your name?\n');
+
+    socket.on('data', function(data) {
+        var data = data.toString();
+        data = data.substring(0, data.length-1);
+
+        if(name == undefined) {
+            name = data;
+            socket.write('Hello, ' + name + '\n');
+            console.log(name + ' has signed in');
+        } else {
+            broadcast(name, data);
+        }
+        //console.log(data);
+    });
+
+    //socket.end();
+});
+
+var port = 3000;
+server.on('listening', function() {
+    console.log('Server now running on ' + port);
+});
+
+server.listen(port);
